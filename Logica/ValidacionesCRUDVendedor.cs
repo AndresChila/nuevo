52d281d7db1bd5c5f7ce15e0b90c98ee6a6af2ba
Andres Chila <andres.ch.quiroga@hotmail.com>
2019-02-23 17:20:47 -0500

    CRUDVendedor

---
+++ Logica/ValidacionesCRUDVendedor.cs
@@ -0,0 +1,274 @@
+﻿using Datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class ValidacionesCRUDVendedor
+    {
+        DAOUsuario dao = new DAOUsuario();
+        Usuario usuario = new Usuario();
+        DataTable usu = new DataTable();
+        DataTable sedess = new DataTable();
+        string cedula, nombre, direccion, telefono, correo, clave, sexo, sede, rol;
+        string cedula0, nombre0, direccion0, telefono0, correo0, clave0, sexo0, sede0, rol0;
+        string mensaje ="todo ok";
+        public ValidacionesCRUDVendedor(string cedula, string nombre, string clave, string direccion, string telefono, string correo,
+                                        string sexo, string sede, string rol, string cedula0, string nombre0, string clave0, string direccion0, 
+                                        string telefono0, string correo0, string sexo0, string sede0, string rol0)
+        {
+            //Constructor
+            this.cedula = cedula;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.correo = correo;
+            this.sexo = sexo;
+            this.sede = sede;
+            this.rol = rol;
+            this.clave = clave;
+            this.cedula0 = cedula0;
+            this.nombre0 = nombre0;
+            this.direccion0 = direccion0;
+            this.telefono0 = telefono0;
+            this.correo0 = correo0;
+            this.sexo0 = sexo0;
+            this.sede0 = sede0;
+            this.rol0 = rol0;
+            this.clave0 = clave0;
+        }
+        bool validarLlenoAgregar()
+        {
+            if (cedula == "" || nombre == "" || clave == "" || direccion == "" || telefono == "" || correo == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool validarNumeros(string num)
+        {
+            try
+            {
+                double x = Convert.ToDouble(num);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool validarCedula()
+        {
+            DataTable cedulatabla = new DataTable();
+
+            cedulatabla = dao.traerUsuarios();
+            for (int i = 0; i < cedulatabla.Rows.Count; i++)
+            {
+                if (cedulatabla.Rows[i]["cedula"].ToString() == cedula)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public string hacerTodoAgregar()
+        {
+            bool resultadoNombre = Regex.IsMatch(nombre, @"^[a-zA-Z]+$");
+            if (validarIngresadoAgregar())
+            {
+                if (validarLlenoAgregar() == true)
+                {
+                    if (resultadoNombre == true)
+                    {
+                        if (validarNumeros(telefono) == true)
+                        {
+                            if (validarNumeros(cedula) == true)
+                            {
+                                if (validarCedula() == true)
+                                {
+
+                                    usuario.Cedula = int.Parse(cedula);
+                                    if (usuario.Cedula <= 0)
+                                    {
+                                        mensaje = "Ingrese los datos de la cédula correctamente.";
+                                        return mensaje;
+                                    }
+                                    usuario.Nombre = nombre;
+                                    usuario.Clave = clave;
+                                    usuario.Direccion = direccion;
+                                    usuario.Telefono = Convert.ToInt64(telefono);
+                                    if (usuario.Telefono <= 0)
+                                    {
+                                        mensaje = "Ingrese los datos del teléfono correctamente.";
+                                        return mensaje;
+                                    }
+                                    usuario.Sexo = sexo;
+
+                                    usuario.Sede = sede;
+                                    usuario.Correo = correo;
+                                    usuario.Estado = 1;
+                                    usuario.Session = "hola";
+                                    usuario.RolId = int.Parse(rol);
+                                    usuario.LastModified = DateTime.Now;
+
+                                    dao.CrearUsuario(usuario);
+                                }
+                                else
+                                {
+                                    dao.agregarUsuarioNuevamente(cedula);
+                                    usu = dao.traerUsuarios2(sede);
+                                    mensaje = "Este usuario ya existe.";
+                                    return mensaje;
+                                }
+                            }
+                            else
+                            {
+                                mensaje = "Ingrese la cedula del Admin correctamente.";
+                                return mensaje;
+                            }
+                        }
+                        else
+                        {
+                            mensaje = "Ingrese el telefono del Admin correctamente.";
+                            return mensaje;
+                        }
+                    }
+                    else
+                    {
+                        mensaje = "Ingrese el nombre del Admin correctamente.";
+                        return mensaje;
+                    }
+                }
+                else
+                {
+                    mensaje = "Ingrese todos los datos.";
+                    return mensaje;
+                }
+            }
+            else
+            {
+                mensaje = "Ya existe";
+                return mensaje;
+            }
+            return mensaje;
+        }
+
+        public Usuario llenarCampos(DataTable usu, string cedula)
+        {
+            for(int i=0; i< usu.Rows.Count; i++)
+            {
+                if(usu.Rows[i]["cedula"].ToString() == cedula)
+                {
+                    usuario.Cedula = long.Parse(usu.Rows[i]["cedula"].ToString());
+                    usuario.Nombre = usu.Rows[i]["nombre"].ToString();
+                    usuario.Clave = usu.Rows[i]["clave"].ToString();
+                    usuario.Direccion = usu.Rows[i]["direccion"].ToString();
+                    usuario.Telefono = long.Parse(usu.Rows[i]["telefono"].ToString());
+                    usuario.Correo = usu.Rows[i]["correo"].ToString();
+                    usuario.RolId = int.Parse(usu.Rows[i]["rol_id"].ToString());
+                }
+            }
+            return usuario;
+        }
+        bool validarLlenoEditar()
+        {
+            if (cedula0 == "" || nombre0 == "" || clave0 == "" || direccion0 == "" || telefono0 == "" || correo0 == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public string hacerTodoEditar()
+        {
+            bool resultadoNombre = Regex.IsMatch(nombre0, @"^[a-zA-Z]+$");
+            if (validarIngresadoEditar())
+            {
+                if (validarLlenoEditar() == true)
+                {
+                    if (resultadoNombre == true)
+                    {
+                        if (validarNumeros(telefono0) == true)
+                        {
+                            Usuario usuario2 = new Usuario();
+
+                            usuario2.Cedula = int.Parse(cedula0);
+                            usuario2.Nombre = nombre0;
+                            usuario2.Clave = clave0;
+                            usuario2.Direccion = direccion0;
+                            usuario2.Estado = 1;
+                            usuario2.Telefono = int.Parse(telefono0);
+                            usuario2.Sexo = sexo0;
+                            usuario2.Sede = sede0;
+                            usuario2.Correo = correo0;
+                            usuario2.Session = "hola";
+                            usuario2.RolId = int.Parse(rol0);
+                            usuario2.LastModified = DateTime.Now;
+
+                            dao.actualizarUsuario(usuario2);
+                        }
+                        else
+                        {
+                            mensaje = "Ingrese el telefono del Admin correctamente.";
+                            return mensaje;
+                        }
+                    }
+                    else
+                    {
+                        mensaje = "Ingrese el nombre del Admin correctamente.";
+                        return mensaje;
+                    }
+                }
+                else
+                {
+                    mensaje = "Ingrese todos los datos. ";
+                    return mensaje;
+                }
+            }
+            else
+            {
+                mensaje = "Ya existe";
+                return mensaje;
+            }
+            return mensaje;
+        }
+        public bool validarIngresadoAgregar()
+        {
+            DataTable cedulatabla = new DataTable();
+
+            cedulatabla = dao.traerUsuarios();
+            for (int i = 0; i < cedulatabla.Rows.Count; i++)
+            {
+                if (cedulatabla.Rows[i]["cedula"].ToString() == cedula && cedulatabla.Rows[i]["estado"].ToString() == "2")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool validarIngresadoEditar()
+        {
+            DataTable cedulatabla = new DataTable();
+
+            cedulatabla = dao.traerUsuarios();
+            for (int i = 0; i < cedulatabla.Rows.Count; i++)
+            {
+                if (cedulatabla.Rows[i]["cedula"].ToString() == cedula && cedulatabla.Rows[i]["estado"].ToString() == "2")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }    
+}
--- WebSite/Controller/Tienda/CRUDVendedor.aspx.cs
+++ WebSite/Controller/Tienda/CRUDVendedor.aspx.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Datos;
+using Logica;
 
 public partial class View_Tienda_CRUDVendedor : System.Web.UI.Page
 {
@@ -36,187 +37,64 @@ public partial class View_Tienda_CRUDVendedor : System.Web.UI.Page
 
     protected void B_Agregar_Click(object sender, EventArgs e)
     {
-        bool resultadoNombre = Regex.IsMatch(TB_Nombre.Text, @"^[a-zA-Z]+$");
+        ValidacionesCRUDVendedor val = new ValidacionesCRUDVendedor(TB_Cedula.Text, TB_Nombre.Text, TB_Clave.Text, TB_Direccion.Text, TB_Telefono.Text, 
+                                                                     TB_Correo.Text, D_Sexo.SelectedValue.ToString(), Session["sede"].ToString(), TB_Rol.Text,
+                                                                     null, null, null, null, null,
+                                                                     null, null, null, null);
 
-        if (validarLlenoAgregar() == true)
-        {
-            if (resultadoNombre == true)
-            {
-                if (validarNumeros(TB_Telefono.Text) == true)
-                {
-                    if (validarNumeros(TB_Cedula.Text) == true)
-                    {
-                        if (validarCedula() == true)
-                        {
+        string a =val.hacerTodoAgregar();
+        
 
-                            usuario.Cedula = int.Parse(TB_Cedula.Text);
-                            if(usuario.Cedula <= 0)
-                            {                               
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                                    RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese los datos de la cédula correctamente.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-                                    return;                                
-                            }
-                            usuario.Nombre = TB_Nombre.Text;
-                            usuario.Clave = TB_Clave.Text;
-                            usuario.Direccion = TB_Direccion.Text;
-                            usuario.Telefono = Convert.ToInt64(TB_Telefono.Text);
-                            if (usuario.Telefono <= 0)
-                            {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                                RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese los datos del teléfono correctamente.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-                                return;
-                            }
-                            usuario.Sexo = D_Sexo0.SelectedValue;
+        usu = dao.traerUsuarios2(Session["sede"].ToString());
+        GV_Productos.DataSource = usu;
+        GV_Productos.DataBind();
+        DropDownList1.Items.Add(TB_Cedula.Text);
+        TB_Cedula.Text = "";
+        TB_Nombre.Text = "";
+        TB_Clave.Text = "";
+        TB_Direccion.Text = "";
+        TB_Telefono.Text = "";
+        TB_Correo.Text = "";
 
-                            usuario.Sede = Session["sede"].ToString();
-                            usuario.Correo = TB_Correo.Text;
-                            usuario.Estado = 1;
-                            usuario.Session = "hola";
-                            usuario.RolId = int.Parse(TB_Rol.Text);
-                            usuario.LastModified = DateTime.Now;
+#pragma warning disable CS0618 // Type or member is obsolete
+        RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('"+a+"');</script>");
+#pragma warning restore CS0618 // Type or member is obsolete
 
-                            dao.CrearUsuario(usuario);
-
-                            TB_Cedula.Text = "";
-                            TB_Nombre.Text = "";
-                            TB_Clave.Text = "";
-                            TB_Direccion.Text = "";
-                            TB_Telefono.Text = "";
-                            TB_Correo.Text = "";
-
-                            usu = dao.traerUsuarios2(Session["sede"].ToString());
-                            GV_Productos.DataSource = usu;
-                            GV_Productos.DataBind();
-                            DropDownList1.Items.Add(TB_Cedula.Text);
-                        }
-                        else
-                        {
-                            dao.agregarUsuarioNuevamente(TB_Cedula.Text);
-                            usu = dao.traerUsuarios2(Session["sede"].ToString());
-                            GV_Productos.DataSource = usu;
-                            GV_Productos.DataBind();
-                            DropDownList1.Items.Add(TB_Cedula.Text);
-
-                            TB_Cedula.Text = "";
-                            TB_Nombre.Text = "";
-                            TB_Clave.Text = "";
-                            TB_Direccion.Text = "";
-                            TB_Telefono.Text = "";
-                            TB_Correo.Text = "";
-
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                            RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Este usuario ya existe.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-                        }
-                    }
-                    else
-                    {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                        RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese la cedula del Admin correctamente.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-                    }
-                }
-                else
-                {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                    RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese el telefono del Admin correctamente.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-                }
-            }
-            else
-            {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese el nombre del Admin correctamente.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-            }
-        }
-        else
-        {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-            RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese todos los datos. ');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-        }
     }
 
     protected void B_Seleccionar_Click(object sender, EventArgs e)
     {
-        for (int i = 0; i < usu.Rows.Count; i++)
-        {
-            if (DropDownList1.SelectedItem.ToString() == usu.Rows[i]["cedula"].ToString())
-            {
-                TB_Cedula0.Text = usu.Rows[i]["cedula"].ToString();
-                TB_Nombre0.Text = usu.Rows[i]["nombre"].ToString();
-                TB_Clave0.Text = usu.Rows[i]["clave"].ToString();
-                TB_Direccion0.Text = usu.Rows[i]["direccion"].ToString();
-                TB_Telefono0.Text = usu.Rows[i]["telefono"].ToString();
+        ValidacionesCRUDVendedor val = new ValidacionesCRUDVendedor(null, null, null, null, null,
+                                                                     null, null, null, null, null, null, null, null, null,
+                                                                     null, null, null, null);
+        usu = dao.traerUsuarios2(Session["sede"].ToString());
+        Usuario usuario = val.llenarCampos(usu, DropDownList1.SelectedItem.ToString());
 
-                TB_Correo0.Text = usu.Rows[i]["correo"].ToString();
-                TB_Rol0.Text = usu.Rows[i]["rol_id"].ToString();
-            }
-        }
+        TB_Cedula0.Text = usuario.Cedula.ToString();
+        TB_Nombre0.Text = usuario.Nombre;
+        TB_Clave0.Text = usuario.Clave;
+        TB_Direccion0.Text = usuario.Direccion;
+        TB_Telefono0.Text = usuario.Telefono.ToString();
+        TB_Correo0.Text = usuario.Correo;
+        TB_Rol0.Text = usuario.RolId.ToString();
+            
     }
 
     protected void B_Actualizar_Click(object sender, EventArgs e)
     {
-        bool resultadoNombre = Regex.IsMatch(TB_Nombre0.Text, @"^[a-zA-Z]+$");
 
-        if (validarLlenoEditar() == true)
-        {
-            if (resultadoNombre == true)
-            {
-                if (validarNumeros(TB_Telefono0.Text) == true)
-                {
-                    Usuario usuario2 = new Usuario();
-
-                    usuario2.Cedula = int.Parse(TB_Cedula0.Text);
-                    usuario2.Nombre = TB_Nombre0.Text;
-                    usuario2.Clave = TB_Clave0.Text;
-                    usuario2.Direccion = TB_Direccion0.Text; usuario2.Estado = 1;
-
-                    usuario2.Telefono = int.Parse(TB_Telefono0.Text);
-                    usuario2.Sexo = D_Sexo.SelectedValue;
-                    usuario2.Sede = Session["sede"].ToString();
-                    usuario2.Correo = TB_Correo0.Text;
-                    usuario2.Session = "hola";
-                    usuario2.RolId = int.Parse(TB_Rol0.Text);
-                    usuario2.LastModified = DateTime.Now;
-
-                    dao.actualizarUsuario(usuario2);
-
-                    TB_Cedula0.Text = "";
-                    TB_Nombre0.Text = "";
-                    TB_Clave0.Text = "";
-                    TB_Direccion0.Text = "";
-                    TB_Telefono0.Text = "";
-                    TB_Correo0.Text = "";
-
-                    usu = dao.traerUsuarios2(Session["sede"].ToString());
-                    GV_Productos.DataSource = usu;
-                    GV_Productos.DataBind();
-                    DropDownList1.Items.Add(TB_Cedula.Text);
-                }
-                else
-                {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                    RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese el telefono del Admin correctamente.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-                }
-            }
-            else
-            {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-                RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese el nombre del Admin correctamente.');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-            }
-        }
-        else
-        {
-#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
-            RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese todos los datos. ');</script>");
-#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
-        }
+        ValidacionesCRUDVendedor val = new ValidacionesCRUDVendedor(null, null, null, null, null,
+                                                                     null, null, null, null,
+                                                                     TB_Cedula0.Text, TB_Nombre0.Text, TB_Clave0.Text, TB_Direccion0.Text, TB_Telefono0.Text,
+                                                                     TB_Correo0.Text, D_Sexo0.SelectedValue.ToString(), Session["sede"].ToString(), TB_Rol0.Text);
+        string a = val.hacerTodoEditar();
+        usu = dao.traerUsuarios2(Session["sede"].ToString());
+        GV_Productos.DataSource = usu;
+        GV_Productos.DataBind();
+        DropDownList1.Items.Add(TB_Cedula.Text);
+#pragma warning disable CS0618 // Type or member is obsolete
+        RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('"+a+"');</script>");
+#pragma warning restore CS0618 // Type or member is obsolete
     }
 
     protected void B_Eliminar_Click(object sender, EventArgs e)
@@ -231,70 +109,9 @@ public partial class View_Tienda_CRUDVendedor : System.Web.UI.Page
         DropDownList1.Items.Remove(DropDownList1.SelectedItem.ToString());
     }
 
-    bool validarLlenoAgregar()
-    {
-        if (TB_Cedula.Text == "" || TB_Nombre.Text == "" || TB_Clave.Text == "" || TB_Direccion.Text == "" || TB_Telefono.Text == "" || TB_Correo.Text == "")
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
+    
 
-    bool validarLlenoEditar()
-    {
-        if (TB_Cedula0.Text == "" || TB_Nombre0.Text == "" || TB_Clave0.Text == "" || TB_Direccion0.Text == "" || TB_Telefono0.Text == "" || TB_Correo0.Text == "")
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
+    
 
-    public bool validarNumeros(string num)
-    {
-        try
-        {
-            double x = Convert.ToDouble(num);
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
-    }
-
-    public bool validarCedula()
-    {
-        DataTable cedula = new DataTable();
-
-        cedula = dao.traerUsuarios();
-        for (int i = 0; i < cedula.Rows.Count; i++)
-        {
-            if (cedula.Rows[i]["cedula"].ToString() == TB_Cedula.Text)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public bool validarIngresado()
-    {
-        DataTable cedula = new DataTable();
-
-        cedula = dao.traerUsuarios();
-        for (int i = 0; i < cedula.Rows.Count; i++)
-        {
-            if (cedula.Rows[i]["cedula"].ToString() == TB_Cedula.Text && cedula.Rows[i]["estado"].ToString() == "2")
-            {
-                return false;
-            }
-        }
-        return true;
-    }
+    
 }
