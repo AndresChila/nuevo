96b4df17d838d676ee0651939006da420e29e3df
Unknown <rrssangie@gmail.com>
2019-02-25 18:41:30 -0500

    NuevoAbono

---
+++ Logica/NuevosAbonos.cs
@@ -0,0 +1,122 @@
+﻿using Datos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilitarios;
+
+namespace Logica
+{
+    public class NuevosAbonos
+    {
+        string mensaje, precioDeuda, pagoActual, idAbono;
+        DataTable vntaS, datosAbono;
+        List<Producto> refresh;
+        public NuevosAbonos(DataTable vntaS, string precioDeuda, string pagoActual, string idAbono)
+        {
+            this.vntaS = vntaS;
+            this.precioDeuda = precioDeuda;
+            this.pagoActual = pagoActual;
+            this.idAbono = idAbono;
+        }
+
+        public string nuevaVenta()
+        {
+            if (vntaS != null)
+            {
+                Producto individual = new Producto();
+                datosAbono = new DataTable();
+                datosAbono = (vntaS as DataTable);
+
+                foreach (DataRow row in datosAbono.Rows)
+                {
+                    Venta venta = new Venta();
+                    venta.Idcliente = Convert.ToInt32(row["idcliente"]);
+                    venta.Producto = JsonConvert.DeserializeObject<List<Producto>>(Convert.ToString(row["descripcion"]));
+                    refresh = venta.Producto as List<Producto>;
+                    venta.Idvendedor = Convert.ToInt32(row["idvendedor"]);
+                    venta.Fecha = Convert.ToDateTime(row["fecha"]);
+                    venta.Precio = Convert.ToDouble(row["precio"]);
+                    venta.Sede = Convert.ToString(row["sede"]);
+                    DAOUsuario dAO = new DAOUsuario();
+                    dAO.crearVenta(venta, JsonConvert.SerializeObject(venta.Producto));
+                    mensaje = "Se ha convertido en venta";
+                    //this.actualizarInventario();
+                }
+
+                
+            }
+            return mensaje;
+        }
+
+        public string traerMensaje()
+        {
+            return mensaje;
+        }
+
+        public string agregarProducto()
+        {
+            if (validarLlenoAbono() == true)
+            {
+                double a, b;
+                a = Convert.ToDouble(precioDeuda);
+                b = Convert.ToDouble(pagoActual);
+                if (idAbono == null)
+                {
+                    mensaje = "Seleccione un abono para actualzar.";
+                }
+                else
+                {
+                    if (a < b)
+                    {
+                        mensaje = "El precio a pagar supera la deuda actual.";
+                    }
+                    else if (b <= a)
+                    {
+                        DAOUsuario dAO = new DAOUsuario();
+                        dAO.actualizarAbono(Convert.ToInt32(idAbono), a - b);
+                        mensaje = "Abono actualizado. Nueva deuda" + (a - b);
+                        //this.llenarGV_Abonos();
+                        if ((a - b) == 0)
+                        {
+                            this.nuevaVenta();
+                        }
+                        pagoActual=null;
+                        precioDeuda=null;
+                        idAbono = null;
+                    }
+                }
+            }
+            else
+            {
+                mensaje = "Ingrese datos.";
+            }
+            return mensaje;
+        }
+
+        public void seleccionarAbono(string comandName, int comandArgument)
+        {
+            if (comandName.Equals("Select"))
+            {
+                DAOUsuario dAO = new DAOUsuario();
+                precioDeuda = Convert.ToString(dAO.traePrecioAbono(Convert.ToInt32(comandArgument)));
+               idAbono = Convert.ToString(comandArgument);
+            }
+        }
+
+        bool validarLlenoAbono()
+        {
+            if (precioDeuda == "" || pagoActual == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
--- WebSite/Controller/Tienda/NuevoAbono.aspx.cs
+++ WebSite/Controller/Tienda/NuevoAbono.aspx.cs
@@ -7,18 +7,22 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Datos;
+using Logica;
+using Utilitarios;
 
 public partial class View_Tienda_NuevoAbono : System.Web.UI.Page
 {
+    DataTable vntaS,refresh;
+    string precioDeuda, pagoActual;
     protected void Page_Load(object sender, EventArgs e)
     {
         llenarGV_Abonos();
     }
 
-    private List<Producto> listaVenta
+    public List<Producto> listaVenta
     {
         get { return Session["refresh"] as List<Producto>; }
-        set { Session["refesh"] = value; }
+        set { Session["refresh"] = value; }
     }
 
     private DataTable datosAbono
@@ -27,15 +31,18 @@ public partial class View_Tienda_NuevoAbono : System.Web.UI.Page
         set { Session["venta"] = value; }
     }
 
+    private string idAbono
+    {
+        get { return Session["idAbono"] as string; }
+        set { Session["idAbono"] = value; }
+    }
+
     protected void GV_AbonosPendientes_RowCommand(object sender, GridViewCommandEventArgs e)
     {
-        if (e.CommandName.Equals("Select"))
-        {
-            DAOUsuario dAO = new DAOUsuario();
-            TB_PrecioDeuda.Text = Convert.ToString(dAO.traePrecioAbono(Convert.ToInt32(e.CommandArgument)));
-            Session["idAbono"] = Convert.ToString(e.CommandArgument);
-        }
+        NuevosAbonos select = new NuevosAbonos(null, null, null, null);
+        select.seleccionarAbono(e.CommandName, int.Parse(e.CommandArgument.ToString()));
     }
+
     void llenarGV_Abonos()  
     {
         DAOUsuario dAO = new DAOUsuario();
@@ -46,36 +53,19 @@ public partial class View_Tienda_NuevoAbono : System.Web.UI.Page
         GV_AbonosPendientes.DataSource = abo;
         GV_AbonosPendientes.DataBind();
     }
+
     void nuevaVenta()
     {
-        if(Session["venta"] != null)
-        {
-            Producto individual = new Producto();
-            datosAbono = new DataTable();
-            datosAbono = (Session["venta"] as DataTable);
-            
-            foreach(DataRow row in datosAbono.Rows)
-            {
-                Venta venta = new Venta();
-                venta.Idcliente = Convert.ToInt32(row["idcliente"]);
-                venta.Producto = JsonConvert.DeserializeObject<List<Producto>>(Convert.ToString(row["descripcion"]));
-                Session["refresh"] = venta.Producto;
-                venta.Idvendedor = Convert.ToInt32(row["idvendedor"]);
-                venta.Fecha = Convert.ToDateTime(row["fecha"]);
-                venta.Precio = Convert.ToDouble(row["precio"]);
-                venta.Sede = Convert.ToString(row["sede"]);
-                DAOUsuario dAO = new DAOUsuario();
-                dAO.crearVenta(venta, JsonConvert.SerializeObject(venta.Producto));
-                
+        vntaS = Session["venta"] as DataTable;
+        NuevosAbonos newAbono = new NuevosAbonos(vntaS, precioDeuda, pagoActual, idAbono);
+        this.actualizarInventario();
+        string a = newAbono.traerMensaje();
 #pragma warning disable CS0618 // Type or member is obsolete
-                RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Se ha convertido en venta.');</script>");
+        RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('" + a + "');</script>");
 #pragma warning restore CS0618 // Type or member is obsolete
-                this.actualizarInventario();
-            }
-            
-            Response.Redirect("../Tienda/VistaFactura.aspx");
-        }
+        Response.Redirect("../Tienda/VistaFactura.aspx");
     }
+
     void actualizarInventario()
     {
         listaVenta = new List<Producto>();
@@ -92,66 +82,19 @@ public partial class View_Tienda_NuevoAbono : System.Web.UI.Page
 
     protected void B_AgregarProducto_Click(object sender, EventArgs e)
     {
-        if (validarLlenoAbono() == true)
-        {
-            double a, b;
-            a = Convert.ToDouble(TB_PrecioDeuda.Text);
-            b = Convert.ToDouble(TB_PagoActual.Text);
-            if (Session["idAbono"] == null)
-            {
+        idAbono = Convert.ToString(Session["idAbono"]);
+        NuevosAbonos newAbono = new NuevosAbonos(vntaS, precioDeuda, pagoActual, idAbono);
+        this.llenarGV_Abonos();
+        string a = newAbono.traerMensaje();
 #pragma warning disable CS0618 // Type or member is obsolete
-                RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Seleccione un abono para actualzar.');</script>");
+        RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('" + a + "');</script>");
 #pragma warning restore CS0618 // Type or member is obsolete
-            }
-            else
-            {
-                if (a < b)
-                {
-#pragma warning disable CS0618 // Type or member is obsolete
-                    RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('El precio a pagar supera la deuda actual.');</script>");
-#pragma warning restore CS0618 // Type or member is obsolete
-                }
-                else if (b <= a)
-                {
-                    DAOUsuario dAO = new DAOUsuario();
-                    dAO.actualizarAbono(Convert.ToInt32(Session["idAbono"]), a - b);
-#pragma warning disable CS0618 // Type or member is obsolete
-                    RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Abono actualizado. Nueva deuda" + (a - b) + "');</script>");
-#pragma warning restore CS0618 // Type or member is obsolete
-                    this.llenarGV_Abonos();
-                    if ((a - b) == 0)
-                    {
-                        this.nuevaVenta();
-                    }
-                    TB_PagoActual.Text = "";
-                    TB_PrecioDeuda.Text = "";
-                    Session["idAbono"] = null;
-                }
-            }
-        }
-        else
-        {
-#pragma warning disable CS0618 // Type or member is obsolete
-            RegisterStartupScript("mensaje", "<script type='text/javascript'>alert('Ingrese datos.');</script>");
-#pragma warning restore CS0618 // Type or member is obsolete
-        }
+
     }
 
     protected void GV_AbonosPendientes_PageIndexChanging(object sender, GridViewPageEventArgs e)
     {
         GV_AbonosPendientes.PageIndex = e.NewPageIndex;
         this.llenarGV_Abonos();
-    }
-
-    bool validarLlenoAbono()
-    {
-        if(TB_PrecioDeuda.Text == "" || TB_PagoActual.Text == "")
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
     }
 }
